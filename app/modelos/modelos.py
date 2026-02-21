dd8a0b38835c4c6a998f34266f69d8071091045d
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2026-02-21 18:02:18 -0500

    Creación de relación Muchos a Muchos entre las clases Album y Canción

--- app/modelos/modelos.py
+++ app/modelos/modelos.py
@@ -1,10 +1,18 @@
-from sqlalchemy import ForeignKey, String, Enum
+from sqlalchemy import ForeignKey, String, Enum, Table, Column
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 import enum
 
 
 class Base(DeclarativeBase):
     pass
+
+
+album_cancion = Table(
+    "album_cancion",
+    Base.metadata,
+    Column("album_id", ForeignKey("albumes.id"), primary_key=True),
+    Column("cancion_id", ForeignKey("canciones.id"), primary_key=True),
+)
 
 
 class Cancion(Base):
@@ -15,6 +23,11 @@ class Cancion(Base):
     minutos: Mapped[int] = mapped_column()
     segundos: Mapped[int] = mapped_column()
     interprete: Mapped[str] = mapped_column(String(128))
+
+    albumes: Mapped[list["Album"]] = relationship(
+        secondary=album_cancion,
+        back_populates="canciones",
+    )
 
     def __repr__(self):
         return "{},{},{},{}".format(
@@ -38,7 +51,11 @@ class Album(Base):
     medio: Mapped[Medio] = mapped_column(Enum(Medio))
     usuario_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"))
 
-    usuario: Mapped["Usuario"] = relationship(back_populates="albums")
+    usuario: Mapped["Usuario"] = relationship(back_populates="albumes")
+    canciones: Mapped[list["Cancion"]] = relationship(
+        secondary=album_cancion,
+        back_populates="albumes",
+    )
 
 
 class Usuario(Base):
@@ -47,6 +64,6 @@ class Usuario(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     nombre_usuario: Mapped[str] = mapped_column(String(50))
     contrasena: Mapped[str] = mapped_column(String(50))
-    albums: Mapped[list["Album"]] = relationship(
+    albumes: Mapped[list["Album"]] = relationship(
         back_populates="usuario", cascade="all, delete-orphan"
     )
