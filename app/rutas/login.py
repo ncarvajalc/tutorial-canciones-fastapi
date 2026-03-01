7c26dcf6f40bae78a20f5451a33e34547495bc48
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2026-03-01 15:35:46 -0500

    Añadida cola de mensajes

--- app/rutas/login.py
+++ app/rutas/login.py
@@ -5,6 +5,7 @@ from app.modelos import Usuario
 from sqlalchemy.orm import Session
 from fastapi.params import Depends
 from app.core.db import get_db
+from app.tareas import registrar_login
 
 router = APIRouter(prefix="/login", tags=["login"])
 
@@ -24,4 +25,5 @@ def login(login: LoginSchema, db: Session = Depends(get_db)):
             status_code=HTTPStatus.UNAUTHORIZED,
             detail="Nombre de usuario o contraseña incorrectos",
         )
+    registrar_login.delay(db_usuario.nombre_usuario)
     return {"mensaje": "Inicio de sesión exitoso"}
